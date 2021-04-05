e78a0e31eec87246622f6b1556e3494a530af1ed
navidfarahmand <navidfarahmand75@gmail.com>
2021-04-05 10:46:53 +0430

    add pipe state enum

---
+++ PipeState.h
@@ -0,0 +1,18 @@
+/*
+ * PipeState.h
+ *
+ *  Created on: Apr 4, 2021
+ *      Author: navidfarahmand
+ */
+
+#ifndef PIPESTATE_H_
+#define PIPESTATE_H_
+
+enum PipeState {
+	OK,
+	NOT_OK
+};
+
+
+
+#endif /* PIPESTATE_H_ */
