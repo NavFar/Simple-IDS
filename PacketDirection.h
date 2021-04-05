cf4d923472e2dd21f0b9268777c22a1958cb4c45
navidfarahmand <navidfarahmand75@gmail.com>
2021-04-05 10:45:51 +0430

    add Packet direction enum

---
+++ PacketDirection.h
@@ -0,0 +1,15 @@
+/*
+ * PacketDirection.h
+ *
+ *  Created on: Apr 4, 2021
+ *      Author: navidfarahmand
+ */
+#ifndef PACKETDIRECTION_H_
+#define PACKETDIRECTION_H_
+
+#include <iostream>
+enum PacketDirection{
+	IN,
+	OUT
+};
+#endif /* PACKETDIRECTION_H_ */
