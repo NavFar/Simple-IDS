34a9fbe90df54066d29d78e70e3363a6adb79d6d
navidfarahmand <navidfarahmand75@gmail.com>
2021-04-05 17:14:32 +0430

    add main function to read all files of a directory

--- SimpleIDS.cpp
+++ SimpleIDS.cpp
@@ -2,29 +2,74 @@
 #include <fstream>
 #include <string>
 #include <bits/stdc++.h>
-#include <curl/curl.h>
-#include <curl/easy.h>
 #include "Packet.h"
 #include "PacketDirection.h"
 #include "TimedDataPipe.h"
 #include "WordSensitiveDataPipe.h"
 #include "vector"
 #include "configs.h"
+#include <filesystem>
+#include <sstream>
 using namespace std;
+namespace	 fs = std::filesystem;
+
+string validateTraffic(string const &);
 int main(int argc, char **argv) {
-	ifstream inputFile ("/home/navidfarahmand/Desktop/sample-traffic.txt", ios::in);
-	string line, word,lineNumber;
+
+	if(argc<2){
+		std::cerr<<"need to get directory address"<<std::endl;
+		return 1;
+	}
+	try{
+	if(!fs::exists(argv[1])){
+		std::cerr<<"no such directory"<<std::endl;
+		return 1;
+	}
+	if(!fs::is_directory(argv[1])){
+		std::cerr<<"not a directory"<<std::endl;
+		return 1;
+	}
+	fs::path trafficDirectory(argv[1]);
+	stringstream temporaryStream;
+	for (const auto & file : fs::recursive_directory_iterator(trafficDirectory))
+//	        cout << file.path() << endl;
+	{
+		temporaryStream <<file.path().filename()<<" --> "<< validateTraffic(file.path())<<endl;
+	}
+	ofstream fileoutput(trafficDirectory /"report.txt",ios::out);
+	fileoutput<<temporaryStream.rdbuf();
+	fileoutput.close();
+	std::cout<<"Report has been created in "<<(trafficDirectory /"report.txt")<<std::endl;
+
+	}catch (exception &exception){
+		std::cerr<<"error reading files"<<std::endl;
+		return 1;
+	}
+	return 0;
+
+
+
+
+	return 0;
+}
+string validateTraffic(string const &  path){
+
+	ifstream inputFile (path, ios::in);
 	TimedDataPipe dosPipe= TimedDataPipe();
 	vector<string> sqlivalues{SQLI_WORDS};
 	vector<string> xssvalues{XSS_WORDS};
-	WordSensitiveDataPipe sqliPipe = WordSensitiveDataPipe(sqlivalues);
-	WordSensitiveDataPipe xssPipe = WordSensitiveDataPipe(xssvalues);
+	WordSensitiveDataPipe sqliPipe = WordSensitiveDataPipe(sqlivalues,SQLI_LIMIT);
+	WordSensitiveDataPipe xssPipe = WordSensitiveDataPipe(xssvalues,XSS_LIMIT);
 	Packet packet;
 	while(!inputFile.eof()){
+		string line, word,lineNumber;
 		std::getline(inputFile,line);
 		istringstream splitStream(line);
 		Packet packet;
 		splitStream>>lineNumber; //read packet number
+		if(lineNumber.empty()){
+			continue;
+		}
 		splitStream>>word; //read packet direction
 		transform(word.begin(),word.end(),word.begin(), ::tolower);
 		if(word == "in"){
@@ -58,14 +103,12 @@ int main(int argc, char **argv) {
 		dosPipe.addToPipe(packet);
 		sqliPipe.addToPipe(packet);
 		xssPipe.addToPipe(packet);
-
-//		cout<<dosPipe.getPipeState()<<endl;
-//		cout<<packet<<endl;
-
-
-
 	}
-	cout<<xssPipe.getPipeState();
-	cout<<sqliPipe.getPipeState();
-	return 0;
+	string result ="DOS attack : ";
+	result +=dosPipe.getPipeState()?"TRUE":"FALSE";
+	result +="\t SQLI attack : ";
+	result +=sqliPipe.getPipeState()?"TRUE":"FALSE";
+	result +="\t XSS attack : ";
+	result +=xssPipe.getPipeState()?"TRUE":"FALSE";
+	return result;
 }
