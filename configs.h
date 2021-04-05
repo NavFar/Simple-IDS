c81ab4b3e83e0b5b8d5b4291855660d38c2f8670
navidfarahmand <navidfarahmand75@gmail.com>
2021-04-05 14:46:51 +0430

    add sqli and xss limit to configs

--- configs.h
+++ configs.h
@@ -2,7 +2,9 @@
 #define CONFIGS_H_
 
 #define SQLI_WORDS	"select", "union", "and 1=1", "or 1=1", "sleep"
+#define SQLI_LIMIT 15
 #define XSS_WORDS "alert", "onerror", "onload", "confirm", "src="
+#define XSS_LIMIT 15
 #define DOS_INTERVAL 1000
 #define DOS_LIMIT	100
 
