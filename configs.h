d9af699cc1c2feba1bb3b9adc25b8f6ea5758836
navidfarahmand <navidfarahmand75@gmail.com>
2021-04-05 14:30:48 +0430

    moved configs to it's own header time

---
+++ configs.h
@@ -0,0 +1,16 @@
+/*
+ * configs.h
+ *
+ *  Created on: Apr 5, 2021
+ *      Author: navidfarahmand
+ */
+
+#ifndef CONFIGS_H_
+#define CONFIGS_H_
+
+#define SQLI_WORDS	"select", "union", "and 1=1", "or 1=1", "sleep"
+#define XSS_WORDS "alert", "onerror", "onload", "confirm", "src="
+#define DOS_INTERVAL 1000
+#define DOS_LIMIT	100
+
+#endif /* CONFIGS_H_ */
