0a971bdca0538edafb301b2d5e32505042bee42d
navidfarahmand <navidfarahmand75@gmail.com>
2021-04-05 10:50:47 +0430

    data pipe to search for pattern in packets

---
+++ WordSensitiveDataPipe.cpp
@@ -0,0 +1,18 @@
+/*
+ * WordSensitiveDataPipe.cpp
+ *
+ *  Created on: Apr 5, 2021
+ *      Author: navidfarahmand
+ */
+
+#include "WordSensitiveDataPipe.h"
+
+WordSensitiveDataPipe::WordSensitiveDataPipe() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WordSensitiveDataPipe::~WordSensitiveDataPipe() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ WordSensitiveDataPipe.h
@@ -0,0 +1,19 @@
+/*
+ * WordSensitiveDataPipe.h
+ *
+ *  Created on: Apr 5, 2021
+ *      Author: navidfarahmand
+ */
+
+#ifndef WORDSENSITIVEDATAPIPE_H_
+#define WORDSENSITIVEDATAPIPE_H_
+
+#include "DataPipe.h"
+
+class WordSensitiveDataPipe: public DataPipe {
+public:
+	WordSensitiveDataPipe();
+	virtual ~WordSensitiveDataPipe();
+};
+
+#endif /* WORDSENSITIVEDATAPIPE_H_ */
